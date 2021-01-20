b894c17083605100476f1d5a4fb352835c31542d
Parag Jyoti Pal <P-arag@github.com>
2021-01-20 17:03:25 +0530

    Cli Feature sorted

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # PyBuster
+  -cn
+    	Show CNAME records (dns mode only, cannot be used with '-i' option)
+
+  -e	Expanded mode, print full URLs
+
+  -f	Append a forward-slash to each directory request (dir mode only)
+
+  -fw
+    	Force continued operation when wildcard found
+
+  -i	Show IP addresses (dns mode only)
+
+  -l	Include the length of the body in the output (dir mode only)
+
+  -m string
+    	Directory/File mode (dir) or DNS mode (dns) (default "dir")
+
+  -n	Don't print status codes
+
+  -np
+    	Don't display progress
+
+  -o string
+    	Output file to write results to (defaults to stdout)
+
+  -p string
+    	Proxy to use for requests [http(s)://host:port] (dir mode only)
+
+  -q	Don't print the banner and other noise
+
+  -r	Follow redirects
+
+  -s string
+    	Positive status codes (dir mode only) (default "200,204,301,302,307,403")
+
+  -t int
+    	Number of concurrent threads (default 10)
+
+  -to duration
+    	HTTP Timeout in seconds (dir mode only) (default 10s)
+
+  -u string
+    	The target URL or Domain
+
+  -v	Verbose output (errors)
+
+  -w string
+    	Path to the wordlist
+
+  -x string
+    	File extension(s) to search for (dir mode only)
 
--- main.py
+++ main.py
@@ -3,36 +3,137 @@
 import requests
 import threading
 from bs4 import BeautifulSoup
+from colors import color
+import click
+import time
+import os
+import socket
+from urllib.parse import urlparse
+import pyfiglet
 
 words = []
 
 
-url = input("Enter a valid url:- ")
-recursive = True
+@click.command()
+@click.option('--url', '-u')
+@click.option('--wordlist', '-w')
+@click.option('-ip/-np', default=False)
+@click.option('--save', '-s')
+@click.option('-f/--nf', default=True)
+@click.option('-v/-nv', default=False)
+def main(url, wordlist, ip, save, f, v):
+    header = color.GREEN+f'''
+########################################################
+{pyfiglet.figlet_format("PyBuster", font='slant')}
+    '''
 
-try:
-    page = requests.get(url)
-except:
-    print("Invalid Url")
-    exit()
+    if url[-1] != "/":
+        url = url + "/"
 
-if url[-1] != "/":
-    url = url + "/"
+    try:
+        page = requests.get(url)
+        header = header+f"""
+########################################################
+## Url       ## {url}
+        """
+    except:
+        print(color.RED+"Invalid Url !!")
+        exit()
 
-
-# http://13.126.7.62/
-
-with open("./wordlists/big.txt") as f:
-    print("getting wordslist")
-    for line in f.readlines():
+    if wordlist is None:
         try:
-            newPage = requests.get(url+line.strip("\n"))
-            print("Got endpoint /"+line.strip("\n"))
-            endpointFile = open("./endpoints.txt", 'a')
-            endpointFile.write(url+line)
-            endpointFile.close()
-            print(url+line.strip("\n"))
-        except KeyboardInterrupt:
-            break
-        except:
-            print(line.strip("\n") + "  Failed")
+            print(
+                color.BLUE+"No wordlist Specified !!, going with default, for custom, try -w <file path>")
+            wordlist = "./wordlists/big.txt"
+
+        except Exception as e:
+            print(color.RED+e)
+
+    if not wordlist.endswith('.txt'):
+        print(color.RED+"A words list must have .txt extension")
+        exit()
+
+    if not os.path.exists(wordlist):
+        print(color.RED+"Invalid wordlist location")
+        exit()
+
+    header = header + f'''
+## Wordslist ## {wordlist}
+    '''
+
+    if ip:
+        try:
+            ipUrl = urlparse(url)
+            ip = socket.gethostbyname(ipUrl.netloc)
+        except Exception as err:
+            ip = err
+
+    header = header + f'''
+## IP        ## {ip}   
+    '''
+
+    if save is not None and not os.path.exists(save):
+        print(color.RED+"Invalid saving location !!")
+        exit()
+
+    elif save is not None and os.path.exists(save):
+        header = header + f'''
+## Save      ## {save}
+        '''
+
+    print(header)
+    time.sleep(3)
+    print(color.BLUE+"PyBuster is starting......")
+    time.sleep(2)
+    buster(url, wordlist, save, f, verbose=v)
+
+
+def buster(url, wordslist, save, fails, verbose):
+    GOT = 0
+    SUS = 0
+    FAILED = 0
+
+    with open(wordslist) as f:
+        print("Getting wordslist\n#############################################################")
+        for line in f.readlines():
+            try:
+                newPage = requests.get(url+line.strip("\n"))
+                if verbose and newPage.status_code == 200:
+                    print(color.GREEN+"Got endpoint /"+line.strip("\n") +
+                          f" Status code:- {newPage.status_code}", end='  ')
+                    print(color.PINK+"BUT it looks like a 404 page tho")
+                    SUS += 1
+                elif verbose and str(newPage.content).count("404") > 0:
+                    print(color.GREEN+"Got endpoint /"+line.strip("\n") +
+                          f" Status code:- {newPage.status_code}", end='  ')
+                    print(color.PINK+"Looks sus, my not be a proper endpoint")
+                    SUS += 1
+
+                else:
+                    print(color.GREEN+"Got endpoint /"+line.strip("\n"))
+                    GOT += 1
+                # print(newPage.content)
+                if save is not None:
+                    endpointFile = open(save, 'a')
+                    endpointFile.write(url+line)
+                    endpointFile.close()
+                # print(url+line.strip("\n"))
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                if fails:
+                    # print(e)
+                    print(color.RED+line.strip("\n") + "  Failed")
+                    FAILED += 1
+    print(color.BLUE(f"Output: \n Got: {GOT}\n Sus: {SUS}\n Failed: {FAILED}"))     
+
+
+if __name__ == "__main__":
+    main()
+
+# url = input(color.GREEN+"Enter a valid url:- ")
+# recursive = True
+
+
+# #
+# #http://13.126.7.62/
